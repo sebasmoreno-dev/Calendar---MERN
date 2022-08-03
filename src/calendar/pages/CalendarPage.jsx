3a18550cafa8b73ad14418cbfbeb9c8cf390125e
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-08-03 12:47:31 -0500

    Added React Big Calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import enUS from 'date-fns/locale/en-US'
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
+
 import { Navbar } from '../'
+
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'Cumpleaños del Jefe',
+  note: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours( new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Sebas'
+  }
+}]
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+
+      <Calendar
+        className='px-3'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{height: 'calc( 100vh - 80px)'}}
+      />
     </>
   )
 }
