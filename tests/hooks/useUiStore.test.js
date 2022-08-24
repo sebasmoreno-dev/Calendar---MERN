b1cfc98498b33b018148c57d014e53f0a124e2dd
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-08-24 10:12:13 -0500

    Testing functions connected to the store

--- tests/hooks/useUiStore.test.js
+++ tests/hooks/useUiStore.test.js
@@ -3,6 +3,7 @@ import { useUiStore } from "../../src/hooks/useUiStore";
 import { store, uiSlice } from "../../src/store";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
+import { act } from "react-dom/test-utils";
 
 
 const getMockStore = ( initialState ) => {
@@ -34,7 +35,19 @@ describe('Pruebas en useUiStore', () => {
     })
   });
 
-  test('', () => {
+  test('openModal debe de colocar true en el isDateModalOpen', () => {
 
+    const mockStore = getMockStore({ isDateModalOpen: false });
+    const { result } = renderHook( () => useUiStore(), {
+      wrapper: ({ children }) => <Provider store={ mockStore }> { children } </Provider>
+    });
+
+    const { isDateModalOpen, openDateModal } = result.current;
+
+    act( () => {
+      openDateModal();
+    })
+
+    console.log({ result: result.current, isDateModalOpen})
   });
 })
